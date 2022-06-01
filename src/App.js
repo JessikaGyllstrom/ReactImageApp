df2d8f4bc9c1ad77a5cabb43aec7fd572287d64a
Jessika <72019869+JessikaGyllstrom@users.noreply.github.com>
2022-06-01 13:16:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-function App() {
-  <div>
-    <h1>Image App</h1>
-  </div>;
-}
+import { useState } from "react";
+import Card from "./Card";
 
+function App() {
+  const [images, setImages] = useState([
+    {url: "http://source.unsplash.com/random/1"}, 
+    {url: "http://source.unsplash.com/random/2"}, 
+    {url: "http://source.unsplash.com/random/3"}, 
+    {url: "http://source.unsplash.com/random/4"}, 
+    {url: "http://source.unsplash.com/random/5"}, 
+    {url: "http://source.unsplash.com/random/6"}, 
+  ]);
+  const getNewImages = () => {
+    setImages([...images, {
+      url: `http://source.unsplash.com/random/${Math.floor(Math.random() * 100)}`
+    }])
+  }
+  const removeImage = () => {
+    setImages(images.slice(0, -1));
+  };
+
+  return (
+    <section className="hero">
+      <div className="cardContainer">
+        <Card images={images}/>
+      </div>
+      <div className="btnContainer">
+        <button onClick={getNewImages}>Add</button>
+        <button onClick={removeImage}>Remove</button>
+      </div>
+    </section> 
+  )
+}
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,11 @@
+const Card = ({images}) => {
+    return (
+        <>
+        {images.map((img, i) => (
+            <div className="card" key={i} style={{background: `url("${img.url}") no-repeat center/cover`}}>
+            </div>
+        ))}
+        </>
+    )
+}
+export default Card;
